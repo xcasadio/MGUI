060e215ed8c95da847ca82501d326ee16a6f93bf
xcasadio <casaprod@hotmail.fr>
2026-04-07 19:01:10 +0200

    fix read/write to System.Windows.Clipboard

--- MGUI.Core/UI/Text/StringClipboard.cs
+++ MGUI.Core/UI/Text/StringClipboard.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace MGUI.Core.UI.Text
 {
@@ -11,8 +8,50 @@ namespace MGUI.Core.UI.Text
 #if WINDOWS
         public string Text
         {
-            get => System.Windows.Clipboard.GetText(System.Windows.TextDataFormat.Text);
-            set => System.Windows.Clipboard.SetText(value);
+            get => ExecuteOnStaThread(() => System.Windows.Clipboard.GetText(System.Windows.TextDataFormat.Text) ?? string.Empty);
+            set => ExecuteOnStaThread(() => System.Windows.Clipboard.SetText(value ?? string.Empty));
+        }
+
+        private static T ExecuteOnStaThread<T>(Func<T> operation)
+        {
+            if (Thread.CurrentThread.GetApartmentState() == ApartmentState.STA)
+            {
+                return operation();
+            }
+
+            T result = default;
+            Exception failure = null;
+            Thread thread = new(() =>
+            {
+                try
+                {
+                    result = operation();
+                }
+                catch (Exception ex)
+                {
+                    failure = ex;
+                }
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+
+            if (failure != null)
+            {
+                throw failure;
+            }
+
+            return result;
+        }
+
+        private static void ExecuteOnStaThread(Action operation)
+        {
+            ExecuteOnStaThread(() =>
+            {
+                operation();
+                return true;
+            });
         }
 #else
         public string Text { get; set; }
